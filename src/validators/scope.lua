f47fc36a350789b9fdd3da2754dd8898d239bb46
Crovitz <maciej.krowicki@gmail.com>
2020-04-09 10:18:30 +0200

    Initial commit

---
+++ src/validators/scope.lua
@@ -0,0 +1,24 @@
+local function validate_scope(allowed_scopes, jwt_claims)
+    if allowed_scopes == nil or table.getn(allowed_scopes) == 0 then
+        return true
+    end
+
+    if jwt_claims == nil or jwt_claims.authorization == nil then
+        return nil, "Missing required authorization claim"
+    end
+
+    for _, curr_scope in pairs(allowed_scopes) do
+        for _, permission in pairs(jwt_claims.authorization.permissions) do
+            for _, scope in pairs(jwt_claims.authorization.permissions.scopes) do
+                if curr_scope == scope then
+                    return true
+                end
+            end
+        end    
+    end
+    return nil, "Missing required scope"
+end
+
+return {
+    validate_scope = validate_scope
+}
